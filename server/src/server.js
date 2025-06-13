71e7136cc8c208366254e268809fc48220b1f485
Ömer Faruk Özkan <omerfarukozkaan@gmail.com>
2025-06-13 12:52:27 +0300

    server.js changed

--- server/src/server.js
+++ server/src/server.js
@@ -65,10 +65,10 @@ app.use("/api/common/feature", commonFeatureRoute);
 app.use(errorMiddleware);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../../client/dist/index.html"));
   });
 }
 
