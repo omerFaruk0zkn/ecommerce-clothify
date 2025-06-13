8ab2521ba6606a629e13c7a8e74230d7e8cdaabc
Ömer Faruk Özkan <omerfarukozkaan@gmail.com>
2025-06-13 14:37:02 +0300

    axios.js changed

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
   withCredentials: true,
   headers: {
     "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
